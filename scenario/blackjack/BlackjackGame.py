845ca09a39a12960de0e8b82e30b1d06bd99ca28
tomasruizt <tomas.ruiz.te@gmail.com>
2018-03-11 06:10:14 +0100

    Groomed the BlackJackGame class

--- scenario/blackjack/BlackjackGame.py
+++ scenario/blackjack/BlackjackGame.py
@@ -20,17 +20,6 @@ class BlackjackGame:
         self._shuffled_deck = shuffled_deck
         self._players = players
 
-    def player_score_under_22(self, player_cards: List[Card]) -> bool:
-        """
-        This method determines whether the Player's score, based on his Cards, is
-        still below 22. In that case, they may still draw, if they want to.
-        This method always considers Aces score to be 1.
-        :param player_cards: the Card(s) this player has already been dealt.
-        :return: A bool value whether Player's score is over 21 or not
-        """
-        player_score = sum([min(card.value) for card in player_cards])
-        return player_score < 22
-
     def play_game(self) -> List[List[Card]]:
         """
         Plays a round of of blackjack with the list of Players that were passed
@@ -41,20 +30,33 @@ class BlackjackGame:
         """
         all_player_hands = []
         for player in self._players:
-            current_player_hand = self.take_turn(player)
+            current_player_hand = self._deal_hand(player)
             all_player_hands.append(current_player_hand)
         return all_player_hands
 
-    def take_turn(self, player: Player) -> List:
+    def _deal_hand(self, player: Player) -> List:
         current_player_hand = []
-        for _ in range(2): self.give_new_card(player, current_player_hand)
-        while self.player_score_under_22(current_player_hand) and player.wants_new_card():
-            self.give_new_card(player, current_player_hand)
+        for _ in range(2):
+            self._give_new_card(player, current_player_hand)
+        while self._player_score_under_22(current_player_hand) and player.wants_new_card():
+            self._give_new_card(player, current_player_hand)
         return current_player_hand
 
-    def give_new_card(self, player: Player, hand: List[Card]):
+    def _give_new_card(self, player: Player, hand: List[Card]):
         if not self._shuffled_deck.cards_remaining():
             raise Exception("No more cards remain for player to keep drawing.")
         new_card = self._shuffled_deck.draw_card()
         player.accept_card(card=new_card)
         hand.append(new_card)
+
+    @staticmethod
+    def _player_score_under_22(player_cards: List[Card]) -> bool:
+        """
+        This method determines whether the Player's score, based on his Cards, is
+        still below 22. In that case, they may still draw, if they want to.
+        This method always considers Aces score to be 1.
+        :param player_cards: the Card(s) this player has already been dealt.
+        :return: A bool value whether Player's score is over 21 or not
+        """
+        player_score = sum([min(card.value) for card in player_cards])
+        return player_score < 22
